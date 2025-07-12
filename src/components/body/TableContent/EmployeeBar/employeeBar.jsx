f629a4affe82e3e7142a128b242069f9035d2dfb
ajaychoudhary654329 <ajaychoudhary654329@gmail.com>
2025-07-12 23:50:05 +0530

    employeeBar changes

--- src/components/body/TableContent/EmployeeBar/employeeBar.jsx
+++ src/components/body/TableContent/EmployeeBar/employeeBar.jsx
@@ -6,6 +6,57 @@ import './employeeBar.css';
 
 const EmployeeBar = ()=>{
 
+
+  const employees = [ {
+
+    name : "Ujjwal Sharma ",
+    id: "660638",
+    code : "A0" ,
+    image : myimage10,
+  } , {
+    name : "Raj Kumar " ,
+    id: "660669",
+    code : " TW " ,
+    image : myimage10 , 
+  } , {
+     name : "Bambam Kumar Pandey",
+     id : " 660677",
+     code : " A0",
+     image : myimage10 
+  } , {
+     name : " Shubham Kumar" ,
+     id: "660684" ,
+     code : "A0" ,
+     image : myimage10 
+  } ,{
+    name : " Mohd Azruddin ",
+    id : "660704 ",
+    code : "TW ",
+    image : myimage10 
+  } ,{
+    name : "Kuldeep Oragon " ,
+    id : " 660705 ",
+    code : " A0",
+    image : myimage10 
+  } , { 
+    name : "Ankit",
+    id : " 660709",
+    code : " A0",
+    image: myimage10 , 
+  } , { 
+    name : "Tanshika ",
+    id : " 665432" , 
+    code : "A1",
+    image: myimage10 
+  } , { 
+    name : "Amisha ",
+    id : " 662332" , 
+    code : "A5",
+    image: myimage10 
+  }
+
+]
+
   return(
   
      <> 
@@ -14,7 +65,7 @@ const EmployeeBar = ()=>{
 
 
         <div className='parentdiv-employeebar'>
-          
+        
          <div className='employee1'>
              
           <img src={myimage10} /> 
@@ -27,11 +78,7 @@ const EmployeeBar = ()=>{
           </div>
            
          </div>
-
-
-
-
-
+         
         </div>
 
 
